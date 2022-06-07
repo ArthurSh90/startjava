e70536e86def201309105d604e0ef97647cde5e8
ArthurSh90 <arthur.sh90@gmail.com>
2022-06-07 11:36:29 +0500

    corrected remarks

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -22,27 +22,27 @@ public class VariablesTheme {
                 "Это ноутбук? - " + isLaptop);
 
         System.out.println("\nЗадача №2. Расчет стоимости товара со скидкой.\n");
-        int productOne = 100;
-        int productTwo = 200;
-        float discountAmount = (productOne + productTwo) * 0.11f;
-        float totalAmountDiscount = productOne + productTwo - discountAmount;
+        int priceGoodsX = 100;
+        int priceGoodsY = 200;
+        float discountAmount = (priceGoodsX + priceGoodsY) * 0.11f;
+        float discountPrice = priceGoodsX + priceGoodsY - discountAmount;
         System.out.println("Сумма скидки - " + discountAmount + " руб\n" + 
-                "Общая стоимость товаров со скидкой - " + totalAmountDiscount + " руб");
+                "Общая стоимость товаров со скидкой - " + discountPrice + " руб");
 
         System.out.println("\nЗадача №3. Вывод на консоль слова JAVA.\n");
-        char symbolOne = 'J';
-        char symbolTwo = 'a';
-        char symbolThree = 'v';
-        char symbolFour = 'V';
-        System.out.println("   " + symbolOne + "    " + symbolTwo + "  " + symbolThree + "     " +
-                symbolThree + "  " + symbolTwo + "   " + "\n" +
-                "   " + symbolOne + "   " + symbolTwo + " " + symbolTwo + "  " + symbolThree + "   " +
-                symbolThree + "  " + symbolTwo + " " + symbolTwo + "  \n" +
-                symbolOne + "  " + symbolOne + "  " + symbolTwo + symbolTwo + symbolTwo + symbolTwo
-                + symbolTwo + "  " + symbolFour + " " + symbolFour + "  " + symbolTwo + symbolTwo +
-                symbolTwo + symbolTwo + symbolTwo + " \n" +
-                " " + symbolOne + symbolOne + "  " + symbolTwo + "     " + symbolTwo + "  " + symbolFour +
-                "  " + symbolTwo + "     " + symbolTwo);
+        char charJ = 'J';
+        char charA = 'a';
+        char charV = 'v';
+        char charUppercaseV = 'V';
+        System.out.println("   " + charJ + "    " + charA + "  " + charV + "     " +
+                charV + "  " + charA + "   " + "\n" +
+                "   " + charJ + "   " + charA + " " + charA + "  " + charV + "   " +
+                charV + "  " + charA + " " + charA + "  \n" +
+                charJ + "  " + charJ + "  " + charA + charA + charA + charA
+                + charA + "  " + charUppercaseV + " " + charUppercaseV + "  " + charA + charA +
+                charA + charA + charA + " \n" +
+                " " + charJ + charJ + "  " + charA + "     " + charA + "  " + charUppercaseV +
+                "  " + charA + "     " + charA);
 
         System.out.println("\nЗадача №4. Отображение min и max значений числовых типов данных.\n");
         byte maxByte = Byte.MAX_VALUE;
@@ -50,81 +50,80 @@ public class VariablesTheme {
         int maxInt = Integer.MAX_VALUE;
         long maxLong = Long.MAX_VALUE;
         System.out.println("byte\nПервоначальное значение - " + maxByte + "\nЗначение после инкремента - "
-                + (maxByte + 1) + "\nЗначение после декремента - " + (maxByte - 1) +
+                + (maxByte++) + "\nЗначение после декремента - " + (--maxByte) +
                 "\nshort\nПервоначальное значение - " + maxShort + "\nЗначение после инкремента - "
-                + (maxShort + 1) + "\nЗначение после декремента - " + (maxShort - 1) + 
+                + (maxShort++) + "\nЗначение после декремента - " + (--maxShort) + 
                 "\nint\nПервоначальное значение - " + maxInt + "\nЗначение после инкремента - "
-                + (maxInt + 1) + "\nЗначение после декремента - " + (maxInt - 1) + 
+                + (maxInt++) + "\nЗначение после декремента - " + (--maxInt) + 
                 "\nlong\nПервоначальное значение - " + maxLong + "\nЗначение после инкремента - "
-                + (maxLong + 1) + "\nЗначение после декремента - " + (maxLong - 1));
+                + (maxLong++) + "\nЗначение после декремента - " + (--maxLong));
 
         System.out.println("\nЗадача №5. Перестановка значений переменных.\n");
-        double firstVar = 1.1d;
-        double secondVar = 2.2d;
+        double num1 = 1.1d;
+        double num2 = 2.2d;
         System.out.println("Перестановка значений переменных с помощью третьей переменной.\n" +
-                "Исходные значения переменных: firstVar = " + firstVar + ", secondVar = " + secondVar);
+                "Исходные значения переменных: num1 = " + num1 + ", num2 = " + num2);
         double tmp;
-        tmp = firstVar;
-        firstVar = secondVar;
-        secondVar = tmp;
-        System.out.println("Новые значения переменных: firstVar = " + firstVar + ", secondVar = " + secondVar +
+        tmp = num1;
+        num1 = num2;
+        num2 = tmp;
+        System.out.println("Новые значения переменных: num1 = " + num1 + ", num2 = " + num2 +
                 "\nПерестановка значений переменных с помощью арифметических операций.\n" +
-                "Исходные значения переменных: firstVar = " + firstVar + ", secondVar = " + secondVar);
-        firstVar = firstVar + secondVar;
-        secondVar = firstVar - secondVar;
-        firstVar = firstVar - secondVar;
-        System.out.println("Новые значения переменных: firstVar = " + firstVar + ", secondVar = " + secondVar +
+                "Исходные значения переменных: num1 = " + num1 + ", num2 = " + num2);
+        num1 += num2;
+        num2 = num1 - num2;
+        num1 -= num2;
+        System.out.println("Новые значения переменных: num1 = " + num1 + ", num2 = " + num2 +
                 "\nПерестановка значений переменных с помощью побитовой операции.\n" +
-                "Исходные значения переменных: firstVar = " + firstVar + ", secondVar = " + secondVar);
-        firstVar = (int) firstVar ^ (int) secondVar;
-        secondVar = (int) firstVar ^ (int) secondVar;
-        firstVar = (int) firstVar ^ (int) secondVar;
-        System.out.println("Новые значения переменных: firstVar = " + (firstVar + 0.2) + ", secondVar = " + (secondVar + 0.1));
+                "Исходные значения переменных: num1 = " + num1 + ", num2 = " + num2);
+        num1 = Math.round(num1) ^ Math.round(num2);
+        num2 = Math.round(num1) ^ Math.round(num2);
+        num1 = Math.round(num1) ^ Math.round(num2);
+        System.out.println("Новые значения переменных: num1 = " + num1 + ", num2 = " + num2);
 
         System.out.println("\nЗадача №6. Вывод символов и их кодов.\n");
-        int firstChar = 35;
-        int secondChar = 38;
-        int thirdChar = 64;
-        int fourChar = 94;
-        int fiveChar = 95;
-        System.out.println("Код символа: " + firstChar + " - символ из ASCII-таблицы: " + (char) firstChar +
-                "\nКод символа: " + secondChar + " - символ из ASCII-таблицы: " + (char) secondChar +
-                "\nКод символа: " + thirdChar + " - символ из ASCII-таблицы: " + (char) thirdChar +
-                "\nКод символа: " + fourChar + " - символ из ASCII-таблицы: " + (char) fourChar +
-                "\nКод символа: " + fiveChar + " - символ из ASCII-таблицы: " + (char) fiveChar);
+        int char35 = 35;
+        int char38 = 38;
+        int char64 = 64;
+        int char94 = 94;
+        int char95 = 95;
+        System.out.println("Код символа: " + char35 + " - символ из ASCII-таблицы: " + (char) char35 +
+                "\nКод символа: " + char38 + " - символ из ASCII-таблицы: " + (char) char38 +
+                "\nКод символа: " + char64 + " - символ из ASCII-таблицы: " + (char) char64 +
+                "\nКод символа: " + char94 + " - символ из ASCII-таблицы: " + (char) char94 +
+                "\nКод символа: " + char95 + " - символ из ASCII-таблицы: " + (char) char95);
 
         System.out.println("\nЗадача №7. Произведение и сумма цифр числа.\n");
         int scrNum = 345;
-        int digitOne = 345 / 100;
-        int digitTwo = (345 / 10) % 10;
-        int digitThree = 345 % 10;
+        int digitOne = scrNum / 100;
+        int digitTwo = (scrNum / 10) % 10;
+        int digitThree = scrNum % 10;
         System.out.println("Сумма цифр числа - " + scrNum + " = " + (digitOne + digitTwo + digitThree) +
                 "\nпроизведение цифр числа - " + scrNum + " = " + (digitOne * digitTwo * digitThree));
 
         System.out.println("\nЗадача №8. Вывод на консоль ASCII-арт Дюка.\n");
-        char rightSlash = 47;
-        char leftSlash = 92;
-        char bottomLine = 95;
-        char leftParenthesis = 40;
-        char rightParenthesis = 41;
+        char slash = '/';
+        char backslash = '\\';
+        char underline = '_';
+        char openBracket = '(';
+        char closeBracket = ')';
         char space = ' ';
-        String firstLine = "" + space + space + space + space + rightSlash + leftSlash + space + space + space + space;
-        String secondLine = "\n" + space + space + space + rightSlash + space + space + leftSlash + space + space + space;
-        String thirdLine = "\n" + space + space + rightSlash + bottomLine + leftParenthesis + space + rightParenthesis + leftSlash + space + space;
-        String fourLine = "\n" + space + rightSlash + space + space + space + space  + space + space + leftSlash + space;
-        String fiveLine = "\n" + rightSlash + bottomLine + bottomLine + bottomLine + bottomLine + rightSlash + leftSlash + bottomLine + bottomLine +leftSlash;
+        String firstLine = "" + space + space + space + space + slash + backslash + space + space + space + space;
+        String secondLine = "\n" + space + space + space + slash + space + space + backslash + space + space + space;
+        String thirdLine = "\n" + space + space + slash + underline + openBracket + space + closeBracket + backslash + space + space;
+        String fourLine = "\n" + space + slash + space + space + space + space  + space + space + backslash + space;
+        String fiveLine = "\n" + slash + underline + underline + underline + underline + slash + backslash + underline + underline +backslash;
         System.out.println(firstLine + secondLine + thirdLine + fourLine + fiveLine);
 
         System.out.println("\nЗадача №9. Отображение количества сотен, десятков и единиц числа.\n");
-        int num = 123;
-        int hundredsNum = num - (num % 100);
-        int unitsNum = num % 10;
-        int tensNum = num - hundredsNum - unitsNum;
-        System.out.println("Число " + num + " содержит: \n" +
-                hundredsNum + " сотен\n" +
-                tensNum + " десятков\n" + 
-                unitsNum + " единиц.");
-
+        int srcNum = 123;
+        int hundredsNum = srcNum / 100;
+        int tensNum = (srcNum % 100) / 10;
+        int onesNum = srcNum % 10;
+        System.out.println("Число " + srcNum + " содержит: \n" +
+                hundredsNum + " сотню\n" +
+                tensNum + " десятка\n" + 
+                onesNum + " единицы");
         System.out.println("\nЗадача №10. Преобразование секунд.\n");
         int scrSec = 86399;
         int hour = scrSec / 3600;
